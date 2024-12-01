f54bba32b7de2e6a9c69f62313c35518687511d1
keybladeSkai <asuleodu@gmail.com>
2024-12-01 09:59:18 +0100

    new commit 7

--- src/script.js
+++ src/script.js
@@ -25,16 +25,6 @@ NavBar.addEventListener("click", () => {
 });
 
 
-// smooth
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    });
-  });
-});
 
